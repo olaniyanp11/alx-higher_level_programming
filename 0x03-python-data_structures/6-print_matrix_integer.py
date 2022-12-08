3bf9a6c8ee27d4db9e9a3d0fe85d711d1e4d625e
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-08 15:27:02 -0800

    task 8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
-#!/usr/bin/pytho n3
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         if row != None:
             for column in row:
-                print("{}".format(column), end=" ")
+                print("{:d}".format(column), end=" ")
             print("")
         else:
             print("")
