a0de1fe239f6f1cd0fb4dc8a0b13711b9a1fb9a2
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-13 17:37:38 -0800

    success

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-for num in range(99):
-    if num < 10:
-        print("0{}".format(num),end="")
-        print("{:c}{:c}".format(44, 32),end="")
-    else:
-        print("{}".format(num),end="") 
-        print("{:c}{:c}".format(44, 32),end="")
-print("{}".format(99)) 
+for num in range(100):
+    if num < 99:
+        print("{:02d}{:c}{:c}".format(num, 44, 32), end="")
+    elif num == 99:
+        print("{}".format(num))
