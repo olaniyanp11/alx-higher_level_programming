d569e9101988e1ed2a18359a75291e09f270ea99
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-15 02:25:59 -0800

    changes in task g and task 7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
-        for k in range(i+1,10):
-            if i < 8:
-                print("{}{}{:c}{:c}".format(i,k,44,32), end="")
-            else:
-                print("{}{}".format(i,k))
+    for k in range(i+1, 10):
+        if i < 8:
+            print("{}{}{:c}{:c}".format(i, k, 44, 32), end="")
+        else:
+            print("{}{}".format(i, k))
