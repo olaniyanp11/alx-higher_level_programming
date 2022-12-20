facf8803d59ee47f0b9ee674d7db2a21cf19d24d
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-19 16:00:57 -0800

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         ans = None
     finally:
-        print("Inside result:{}".format(ans))
+        print("Inside result: {}".format(ans))
         return ans
