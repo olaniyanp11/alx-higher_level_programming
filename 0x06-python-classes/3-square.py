b2ac29ee1ba4ef025b09ab85c0885de2fbf8b027
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-23 01:34:50 +0100

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,14 +15,15 @@ class Square:
             pass
         if ((isinstance(size, int)) is not True):
             raise TypeError("size must be an integer")
-        if size < 0:                     
+        if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         """
         Args:
              self :
                 create an instance
-             Return : 
+             Return :
                 the current area of the  square
         """
         if self.__size == 0:
