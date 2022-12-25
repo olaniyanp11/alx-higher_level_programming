75832dffe445d1976cba4f7c5eb9a2ff30dac918
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-25 12:53:58 +0100

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -11,16 +11,15 @@ class Square:
         return pow(self.__size, 2)
 
     @property
-    def position(self):
+    def size(self):
         return self.__size
 
     @size.setter
-    def position(self, value):
+    def size(self, value):
         if isinstance(value, int):
             self.__size = value
         else:
             raise TypeError("size must be an integer")
-
         if value < 0:
             raise ValueError("size must be >= 0")
 
