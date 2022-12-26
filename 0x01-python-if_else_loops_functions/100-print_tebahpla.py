cb2e98a0a3f85bf8bc1b9e8176d87c1766f4ef88
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-26 08:03:50 +0100

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(0,26,2):
-    print("{:c}{:c}".format((122 - i),(89 - i)), end="")
+for i in range(0, 26, 2):
+    print("{:c}{:c}".format((122 - i), (89 - i)), end="")
