30b0c59f5327fabed52814771271619795fed5e5
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-27 01:24:55 +0100

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    div = 0
+    value = 0
+    for i in range(0,len(my_list)):
+       value += my_list[i][0] * my_list[i][1]
+       div += my_list[i][1]
+    return value / div
+
+
