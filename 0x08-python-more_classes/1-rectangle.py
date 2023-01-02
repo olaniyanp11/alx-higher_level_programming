7448594d2481c72895278635251068f3073feecc
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-03 00:49:26 +0100

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,8 @@ class Rectangle:
     a class that gets the width and height of rectanle
     """
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def height(self):
