1ff4a28dcf1ca94516956ed4c637e2aadad6e85d
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-03 00:01:41 +0100

    task 0 and one

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""creating a class that  defines a string
+"""
+class Rectangle:
+    """
+    an empty class
+    """
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""creating a class that  defines a string
+"""
+class Rectangle:
+    """
+    an empty class
+    """
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            self.__height = value
+        else:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+    def __str__(self):
+            Square.my_print(self)
+            return ""
