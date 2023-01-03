379313807e14e51ba48d6ed497bf9291d47f3934
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-03 02:54:41 +0100

    task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -70,4 +70,4 @@ class Rectangle:
             return str()
 
     def __repr__(self):
-        return f'Rectangle({self.__width},{self.__height})'
+        return f'Rectangle({self.__width}, {self.__height})'
