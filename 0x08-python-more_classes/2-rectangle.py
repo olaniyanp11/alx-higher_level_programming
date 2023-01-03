12740ccf92e0a1a28d4513605b6c223d120c50f0
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-03 01:02:44 +0100

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -45,4 +45,6 @@ class Rectangle:
     def area(self):
         return self.__width * self.__height
     def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
