e1b8673b1bd1dd36707f1483d0f550e5f86bc1b3
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-10 18:08:32 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, "r", encryption="UTF8") as fil:
+    with open(filename, "r", encoding="utf-8") as fil:
         for line in fil:
             print(line, end='')
