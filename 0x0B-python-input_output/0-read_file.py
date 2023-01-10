9ef98430e4d9b29f6bdd43250d60e6b91ff934d2
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-10 18:22:27 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+function that reads a text file
+"""
+
+
 def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout
+        Returns none
+    """
     with open(filename, "r", encoding="utf-8") as fil:
         for line in fil:
             print(line, end='')
