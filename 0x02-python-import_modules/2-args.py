9ec757cca8213efa71934d6768c86fca0a03bdd1
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-28 15:41:48 +0100

    tasks udated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("{} arguments.".format(len(sys.argv)-1))
-elif len(sys.argv) == 2:
-    print("{} argument :".format(len(sys.argv) -1))
-    print("1: {}".format(sys.argv[1]))
-if len(sys.argv) > 1:
-    for i in range(1,len(sys.argv[i])):
-        print("{}: {}".format(i,len(sys.argv[i])))
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    if len(sys.argv) > 1:
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
