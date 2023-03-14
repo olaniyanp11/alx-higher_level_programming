333d785cf7a44489e9ed6acca9117ed17e348ea4
olaniyanp11 <olaniyanp11@gmail.com>
2023-03-14 01:51:39 +0000

    javascript

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
-let num = process.argv[2];
-for (let i = 0; i < num; i++)
-{
-	for (let j = 0; j < num; j++)
-	{
-		process.stdout.write("x");
-	}
-		process.stdout.write("\n");
+let firstArg = process.argv[2];
+if (!firstArg || isNaN(firstArg)) {
+  console.log('Missing size');
+} else {
+  firstArg = parseInt(firstArg);
+  for (let i = 0; i < firstArg; i++) {
+    console.log('X'.repeat(firstArg));
+  }
 }
