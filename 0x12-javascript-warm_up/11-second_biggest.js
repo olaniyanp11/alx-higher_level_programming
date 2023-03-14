5610c9408f7d2ba3da4e49cadbcff1fa0d54a626
olaniyanp11 <olaniyanp11@gmail.com>
2023-03-14 22:03:40 +0000

    javascript 2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
-let number = process.argv[2]
+let number = process.argv[2];
 
 number = parseInt(number);
 
-if (isNaN(process.argv[2]) || number === 1)
-{
-	number = 0
+if (isNaN(process.argv[2]) || number === 1) {
+  number = 0;
 }
 
+let ans = number;
 
-for (let i = 3; i < process.argv.length; i++)
-{
-	let compare = parseInt(process.argv[i]);
+for (let i = 3; i < process.argv.length; i++) {
+  const compare = parseInt(process.argv[i]);
 
-	if (number < compare)
-	{
-		number = compare
-	}
+  if (number < compare) {
+    ans = number;
+    number = compare;
+  }
 }
-console.log(number)
+console.log(ans);
