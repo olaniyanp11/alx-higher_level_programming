4aef43be4276306bfd445817e84e5b39f62d6770
olaniyanp11 <olaniyanp11@gmail.com>
2023-03-14 07:30:44 +0000

    tasks corrected

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-let num = process.argv[2];
-const myVar = '“C is fun”';
-if (num)
-{
-	for (let i = 0; i < num; i++)
-	{
+const num = process.argv[2];
+const myVar = 'C is fun';
+if (num) {
+  for (let i = 0; i < num; i++) {
   	console.log(myVar);
-	}
-}
-else if (!num)
-{
-  	console.log("Missing number of occurrences");
+  }
+} else if (!num) {
+  	console.log('Missing number of occurrences');
 }
