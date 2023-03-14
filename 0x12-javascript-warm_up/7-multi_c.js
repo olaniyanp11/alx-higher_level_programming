3cc703327f98372209309249d2eb4a41bf47fffd
olaniyanp11 <olaniyanp11@gmail.com>
2023-03-14 07:46:16 +0000

    tak 100

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,8 @@ const num = process.argv[2];
 const myVar = 'C is fun';
 if (num) {
   for (let i = 0; i < num; i++) {
-  	console.log(myVar);
+    console.log(myVar);
   }
 } else if (!num) {
-  	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
