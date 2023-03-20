1a97306915b0bf095587ecb7d075e412ef70f385
olaniyanp11 <olaniyanp11@gmail.com>
2023-03-20 08:48:52 +0000

    corrrections made on regex

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name REGEXP '^N...'")
+    cursor.execute("SELECT * FROM states WHERE name REGEXP '^(N)...'")
     states = cursor.fetchall()
     for state in states:
         print(state)
