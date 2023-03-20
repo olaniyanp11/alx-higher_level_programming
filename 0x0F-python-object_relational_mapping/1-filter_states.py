0dda4b0fd69e8f5550ac6539fcb599bb448eba5d
olaniyanp11 <olaniyanp11@gmail.com>
2023-03-20 07:58:00 +0000

    tasks 1&2

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name REGEXP '^N...'")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
