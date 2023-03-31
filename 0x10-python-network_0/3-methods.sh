b1f5aee022215f0780d063536c469496cd597e13
olaniyanp11 <olaniyanp11@gmail.com>
2023-03-31 00:16:27 +0000

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  a Bash script that takes in a URL and displays all HTTP methods the server will accept
-curl -Is "$1" { grep -i allow
+curl -sI "$1" | grep "Allow:" | cut -d ' ' -f2-
