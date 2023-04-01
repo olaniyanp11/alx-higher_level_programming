adecabddbeaf831d819c358d01e536a965b8aaad
olaniyanp11 <olaniyanp11@gmail.com>
2023-04-01 00:45:49 +0000

    get request with header of :X-School-User-Id: 98

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sH "X-School-User-Id: 98" "$1"
+
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Send GET request with custom header
+response=$(curl -s -H "X-School-User-Id: 98" "$1")
+
+# Display response body
+echo "$response"
