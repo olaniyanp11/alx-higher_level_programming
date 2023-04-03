116496cba562539d44011a1cbe1326025723a9d3
olaniyanp11 <olaniyanp11@gmail.com>
2023-04-03 22:50:49 +0000

    tasks 4 and 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s 0.0.0.0:5000/route_6 -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -s "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
