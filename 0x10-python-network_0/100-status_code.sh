7db55dac1b3e373182f53928fe319edd8c07e4ae
olaniyanp11 <olaniyanp11@gmail.com>
2023-04-04 09:59:27 +0000

    task 6-9

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -sI -o dev/bin -w '%{http_code}\n' 0.0.0.0:5000
+# Sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
